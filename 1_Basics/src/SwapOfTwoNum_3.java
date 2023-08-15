340016dd8a713477612d53e8a7afceeb4f751e0e
MdAshfaque01 <kashfaque557@gmail.com>
2023-08-15 16:26:23 +0530

    swap of two number program

---
+++ 1_Basics/src/SwapOfTwoNum_3.java
@@ -0,0 +1,14 @@
+public class SwapOfTwoNum_3 {
+
+    public static void main(String[] args) {
+
+        int num1 = 2;
+        int num2 = 3;
+
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println(num1+"   "+num2);
+
+    }
+}
