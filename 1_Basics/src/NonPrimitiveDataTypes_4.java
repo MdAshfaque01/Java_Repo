02ab59439fdc9cb3a52027635d34d65b51f940d6
MdAshfaque01 <kashfaque557@gmail.com>
2023-08-15 16:46:45 +0530

    non primitive done

---
+++ 1_Basics/src/NonPrimitiveDataTypes_4.java
@@ -0,0 +1,24 @@
+public class NonPrimitiveDataTypes_4 {
+    public static void main(String[] args) {
+        /*
+          1 -> Non-primitives type
+          variables are always references.
+
+
+          2 -> Memory for non-primitives is always
+          allocated on heap.
+
+
+          3 -> Member of Non-primitives get default
+          values (like int variable get 0, boolean
+          get false etc...)
+
+         */
+
+
+        /*
+          if we do not initialize primitive we will get
+          error
+         */
+    }
+}
