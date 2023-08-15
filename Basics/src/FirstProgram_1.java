8413ac84e223f80868b441cda867868637362e66
MdAshfaque01 <kashfaque557@gmail.com>
2023-08-15 16:00:33 +0530

    started basics of java

---
+++ Basics/src/FirstProgram_1.java
@@ -0,0 +1,13 @@
+public class FirstProgram_1 {
+    // java hello world program
+
+
+    /*
+  this is the main function this
+  where the execution of a java
+  program begins
+ */
+   public static void main(String[] args) {
+       System.out.println("Hello world :)");
+   }
+}
