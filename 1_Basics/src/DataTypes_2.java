081cf49225f79d4dac2d5dfde4aa2791c44f1e88
MdAshfaque01 <kashfaque557@gmail.com>
2023-08-15 16:29:48 +0530

    all added

--- 1_Basics/src/DataTypes_2.java
+++ 1_Basics/src/DataTypes_2.java
@@ -3,5 +3,48 @@ public class DataTypes_2 {
 
     public static void main(String[] args){
         int age = 45;
+        String name = "Sandeep";
+        float weight = 70.5f;
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(weight);
     }
 }
+
+// Variable Naming
+
+/*
+    valid Names:
+    ab,Ab,aBc,a1,b12,_a,a_b
+    ab$,$ab
+
+     invalid Name:
+     ||a, a# , a! , else,do ,....
+ */
+
+
+//Data Types
+
+//types of data types
+
+//  i) primitive (int ,char,boolean,byte,...)
+
+   /*
+        boolean -> true/false
+        byte -> -128 to 127 (-2^7 to 2^7 -1)
+        short -> (-2^15 to 2^15 -1)
+        int  -> (-2^31 to 2^31 -1)
+        long -> (-2^63 to 2^63 -1)
+        float -> single precision 32 bit
+        double -> double precision 32 bit
+        char -> 16 bit unicode characters
+
+    */
+
+
+
+
+
+
+
+//    ii) Non Primitive (String , Array,Any user defined class types)
